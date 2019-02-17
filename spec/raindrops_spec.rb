a84bae4921c8b686e3b06bae91212889c154248d
becc-mu <becc.mulugeta@gmail.com>
2019-02-17 15:09:26 +0000

    test for error

--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -8,4 +8,7 @@ describe Raindrops do
   it 'should respond to raindrops(number)' do
     expect(subject).to respond_to(:convert).with(1). argument
   end
+  it 'raise error when input is not number' do
+    expect { subject.convert('abd') }.to raise_error 'You must enter a number!'
+  end
 end
