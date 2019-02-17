7c520279a7a14a704310e83e30958e8e2726caa4
becc-mu <becc.mulugeta@gmail.com>
2019-02-17 16:39:38 +0000

    second user story with pass test

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -5,13 +5,22 @@ class Raindrops
     raise 'Number must be positive' if number < 0
 
     factor_num = 1 if number == 1
-
-    if (number % 3).zero?
+    if (number % 3).zero? && (number % 5).zero? && (number % 7).zero?
+      factor_num += 'PlingPlangPlong'
+    elsif (number % 3).zero? && (number % 5).zero?
+      factor_num += 'PlingPlang'
+    elsif (number % 3).zero? && (number % 7).zero?
+      factor_num += 'PlingPlong'
+    elsif (number % 5).zero? && (number % 7).zero?
+      factor_num += 'PlangPlong'
+    elsif (number % 3).zero?
       factor_num += 'Pling'
     elsif (number % 5).zero?
       factor_num += 'Plang'
     elsif (number % 7).zero?
       factor_num += 'Plong'
+    else
+      factor_num = number
     end
     factor_num
   end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -18,17 +18,35 @@ describe Raindrops do
     it 'returns number when passed with no factors' do
       expect(subject.convert(1)).to eq(1)
     end
-
-    describe '#factors' do
-      it 'return Pling if the number has a factor of 3' do
-        expect(subject.convert(3)).to eq('Pling')
-      end
-      it 'return Plang if the number has a factor of 5' do
+  end
+  describe '#factors' do
+    it 'return Pling if the number has a factor of 3' do
+      expect(subject.convert(3)).to eq('Pling')
+    end
+    it 'return Plang if the number has a factor of 5' do
       expect(subject.convert(5)).to eq('Plang')
-      end
-      it 'return Plong if the number has a factor of 7' do
-        expect(subject.convert(7)).to eq('Plong')
-      end
+    end
+    it 'return Plong if the number has a factor of 7' do
+      expect(subject.convert(7)).to eq('Plong')
+    end
+    it 'return number if no factors' do
+      expect(subject.convert(34)).to eq(34)
     end
   end
+
+  describe '#factors concat' do
+    it 'returns PlingPlang when passed a factor of 3 and 5' do
+      expect(subject.convert(15)).to eq('PlingPlang')
+    end
+    it 'returns PlingPlong when passed a factor of 3 and 7' do
+      expect(subject.convert(21)).to eq('PlingPlong')
+    end
+    it 'returns PlangPlong when passed a factor of 5 and 7' do
+      expect(subject.convert(35)).to eq('PlangPlong')
+    end
+    it 'returns PlingPlong when passed a factor of 3, 5 and 7' do
+      expect(subject.convert(105)).to eq('PlingPlangPlong')
+    end
+  end
+
 end
