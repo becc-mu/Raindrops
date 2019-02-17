5300f47fc3de5e250368920d62fda9bcde039664
becc-mu <becc.mulugeta@gmail.com>
2019-02-17 16:17:08 +0000

    partial test first user story

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -3,6 +3,12 @@ class Raindrops
     factor_num = ''
     raise 'You must enter a number!' unless number.is_a? Numeric
     raise 'Number must be positive' if number < 0
+
     factor_num = 1 if number == 1
+
+    if (number % 3).zero?
+      factor_num += 'Pling'
+    end
+    factor_num
   end
 end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -2,19 +2,28 @@ require 'raindrops'
 describe Raindrops do
   subject(:raindrops) { described_class.new }
 
-  it '.create new Raindrops class' do
-    expect(described_class).to be(Raindrops)
+  describe '#create' do
+    it '.create new Raindrops class' do
+      expect(described_class).to be(Raindrops)
+    end
+    it 'should respond to raindrops(number)' do
+      expect(subject).to respond_to(:convert).with(1). argument
+    end
+    it 'raise error when input is not number' do
+      expect { subject.convert('abd') }.to raise_error 'You must enter a number!'
+    end
+    it 'raise error when input is less than 1' do
+      expect { subject.convert(-5) }.to raise_error 'Number must be positive'
+    end
+    it 'returns number when passed with no factors' do
+      expect(subject.convert(1)).to eq(1)
+    end
+
+    describe '#factors' do
+      it 'return Pling if the number has a factor of 3' do
+        expect(subject.convert(3)).to eq('Pling')
+      end
+    end
   end
-  it 'should respond to raindrops(number)' do
-    expect(subject).to respond_to(:convert).with(1). argument
-  end
-  it 'raise error when input is not number' do
-    expect { subject.convert('abd') }.to raise_error 'You must enter a number!'
-  end
-  it 'raise error when input is less than 1' do
-    expect { subject.convert(-5) }.to raise_error 'Number must be positive'
-  end
-  it 'returns number when passed with no factors' do
-    expect(subject.convert(1)).to eq(1)
-  end
+
 end
