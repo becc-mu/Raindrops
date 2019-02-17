519ab70f6bcaba7b1d61e0ad7a782dc532718646
becc-mu <becc.mulugeta@gmail.com>
2019-02-17 19:40:15 +0000

    refactor to reduce complexity metrics

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,27 +1,26 @@
 class Raindrops
-  def convert(number)
-    factor_num = ''
-    raise 'You must enter a number!' unless number.is_a? Numeric
-    raise 'Number must be positive' if number < 0
 
+  def convert(number)
+    number_is_a?(number)
+    number_is_not_zero?(number)
+    check_factor(number)
+  end
+
+  def check_factor(number)
+    factor_num = ''
     factor_num = 1 if number == 1
-    if (number % 3).zero? && (number % 5).zero? && (number % 7).zero?
-      factor_num += 'PlingPlangPlong'
-    elsif (number % 3).zero? && (number % 5).zero?
-      factor_num += 'PlingPlang'
-    elsif (number % 3).zero? && (number % 7).zero?
-      factor_num += 'PlingPlong'
-    elsif (number % 5).zero? && (number % 7).zero?
-      factor_num += 'PlangPlong'
-    elsif (number % 3).zero?
-      factor_num += 'Pling'
-    elsif (number % 5).zero?
-      factor_num += 'Plang'
-    elsif (number % 7).zero?
-      factor_num += 'Plong'
-    else
-      factor_num = number
-    end
+    factor_num += 'Pling' if (number % 3).zero?
+    factor_num += 'Plang' if (number % 5).zero?
+    factor_num += 'Plong' if (number % 7).zero?
+    factor_num = number if factor_num == ''
     factor_num
+  end
+
+  def number_is_a?(number)
+    raise 'You must enter a number!' unless number.is_a? Integer
+  end
+
+  def number_is_not_zero?(number)
+    raise 'Number must be positive' if number < 0
   end
 end
